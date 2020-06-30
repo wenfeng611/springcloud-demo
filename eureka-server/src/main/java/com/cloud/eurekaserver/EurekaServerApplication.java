f17c6499746add3a1ab44b85c01c8e598f6c6501
wenfeng.zhu <wenfeng.zhu@ketianyun.com>
2020-06-30 15:13:09 +0800

    eureka server demo

---
+++ eureka-server/src/main/java/com/cloud/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.cloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+//启动一个服务注册中心
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
