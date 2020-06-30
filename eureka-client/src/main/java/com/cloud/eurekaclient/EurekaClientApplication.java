ecbc41ab5db6394d229c2268c1c6a392892cadf3
wenfeng.zhu <wenfeng.zhu@ketianyun.com>
2020-06-30 15:44:25 +0800

    eureka-client-demo

---
+++ eureka-client/src/main/java/com/cloud/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,18 @@
+package com.cloud.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+
+
+
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+
+}
