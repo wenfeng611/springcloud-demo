b76dd033310e9c3b7e7340d392a1589af017cda8
wenfeng.zhu <wenfeng.zhu@ketianyun.com>
2020-07-01 11:01:09 +0800

    product-service enable eureka client

--- product-service/src/main/java/com/cloud/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/cloud/productservice/ProductServiceApplication.java
@@ -2,9 +2,11 @@ package com.cloud.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //服务提供商 供其他服务调用 需要注册到eureka
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ProductServiceApplication {
 
