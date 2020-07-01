8109b922eb1ce96f63260e5b4b95bd75f6f2f3d5
wenfeng.zhu <wenfeng.zhu@ketianyun.com>
2020-07-01 09:55:15 +0800

    spring security using eureka

--- eureka-client/src/main/java/com/cloud/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/cloud/eurekaclient/EurekaClientApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
-
-
+//项目用到spring security的时候要打开config目录下面的配置
 @EnableEurekaClient
 @SpringBootApplication
 public class EurekaClientApplication {
