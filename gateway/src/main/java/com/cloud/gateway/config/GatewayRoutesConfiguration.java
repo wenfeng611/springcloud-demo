1dbf99c23033f225c1bf60b81ac33706e0911f79
wenfeng.zhu <wenfeng.zhu@ketianyun.com>
2020-07-03 11:12:37 +0800

    gateway ratelimit test

--- gateway/src/main/java/com/cloud/gateway/config/GatewayRoutesConfiguration.java
+++ gateway/src/main/java/com/cloud/gateway/config/GatewayRoutesConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class GatewayRoutesConfiguration {
 
     //java配置路由
---
+++ gateway/src/main/java/com/cloud/gateway/config/KeyResolverConfiguration.java
@@ -0,0 +1,22 @@
+package com.cloud.gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class KeyResolverConfiguration {
+
+    //请求路劲限流规则
+    @Bean
+    public KeyResolver pathKeyResolver() {
+        return new KeyResolver() {
+            @Override
+            public Mono<String> resolve(ServerWebExchange exchange) {
+                return Mono.just(exchange.getRequest().getPath().toString());
+            }
+        };
+    }
+}
